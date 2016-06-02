96fe7ad25497d8c3bbd51867f7016c941889cedb
Mugeri <nyotuo17@gmail.com>
2016-06-02 15:53:33 +0300

    Got this one after awhile

--- map.js
+++ map.js
@@ -1,33 +1,31 @@
 var http = require('http'),
 	async = require('async');
+	var thearray = [];
 	
 
-	async.map([process.argv[2], process.argv[3]], function(url, toDisplay){
+	async.map([process.argv[2], process.argv[3]], function(url, done){
 		var body = '';
-		console.log("I've gotten this " + url);
 		//use the url in the array
 		http.get(url, function(res){
 
 			//while content is loading append it to the variable body
 			res.on('data', function(chunk){
 				body += chunk.toString();
-				console.log("and the body is " + body);
 
+				
 			});
 
 			res.on('end', function(){
-				toDisplay(null, body);
+				
+				 done(null,body);
+				 //	console.log(thearray);
 			});
-
-		}).on('error', function(err){//check if there is an error
-			toDisplay(err);// pass the error to the display function
-		});
-
-		function toDisplay(err, result){
-			if (err){
-				return console.log(err);//display the error
-			}
-			console.log(result);
 			
-		}
-	});
+		 })
+		 }, function(err, result){
+		 	if (err){
+		 		console.log(err);//display the error
+		 	}
+		 	console.log(result);
+
+		}); 
